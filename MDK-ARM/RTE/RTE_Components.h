77cfb469deb1e2fb968399afee848ee7bdec461c
Caleb KANG <6149677+CalebKang@users.noreply.github.com>
2023-12-12 16:58:47 +0900

    Add files via upload

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '20231212_stm32g4_boot_xmodem' 
+ * Target:  '20231212_stm32g4_boot_xmodem' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
